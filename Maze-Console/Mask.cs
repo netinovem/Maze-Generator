f3e8d4feeadf498ad12ef5a381433a165f717c7b
netinovem <netianovem@gmail.com>
2025-02-04 00:50:02 +0330

    implemented a masking class (wip)

---
+++ Maze-Console/Mask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maze_Console
+{
+    internal class Mask
+    {
+        public int rows { get; private set; }
+        public int columns { get; private set; }
+
+        public bool[,] bits;
+
+        public Mask(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+            bits = new bool[rows, columns];
+
+            //initialize all bits to true
+            foreach (var row in Enumerable.Range(0, rows))
+            {
+                foreach (var column in Enumerable.Range(0, columns))
+                {
+                    bits[row, column] = true;
+                }
+            }
+        }
+
+
+    }
+}
--- Maze-Console/Program.cs
+++ Maze-Console/Program.cs
@@ -8,6 +8,8 @@ namespace Maze_Console
         private static void Main(string[] args)
         {
             ColoredGrid coloredGrid = new ColoredGrid(40, 40);
+            //coloredGrid[0, 0].east.west = null;
+            //coloredGrid[0, 0].south.north = null;
             RecursiveBacktracker.On(coloredGrid);
             Cell start = coloredGrid[coloredGrid.rows / 2, coloredGrid.columns / 2];
             Distances distanceMap = start.Distances();
@@ -17,7 +19,8 @@ namespace Maze_Console
             img.SaveAsPng("C:\\Users\\netinova\\Desktop\\Maze\\Maze-Console\\generated-mazes\\maze3.png");
             Process.Start(new ProcessStartInfo("C:\\Users\\netinova\\Desktop\\Maze\\Maze-Console\\generated-mazes\\maze3.png") { UseShellExecute = true });
 
-            //DeadEndsCountTest.RunTest(100, 20);
+            //DeadEndsCountTest.RunTest(100, 20); 
+
         }
     }
 }
